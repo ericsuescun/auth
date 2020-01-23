210231a29dc1d878531a7b061ef7ce9fdabd07a2
Eric Suescun <edsuescun@gmail.com>
2020-01-23 17:05:29 -0500

    Se agrega el bcrypt

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 var cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session');
+var bcrypt = require('bcrypt');
 var User = require('./models/user');	//Importo el modelo creado para User
 var routes = require('./routes');	//Importo el enrutador
 
@@ -9,6 +11,10 @@ app.set('views', './views');
 
 app.use(express.urlencoded());
 app.use(cookieParser());
+app.use(cookieSession({
+	secret: "ABCDEFGH",
+	maxAge: 24 * 60 * 60 * 1000
+}));
 app.use('/', routes);	//Agrego a la ruta / (sin prefijo) lo que tengo en routes.js
 
 app.listen(3000, console.log('Listening on port 3000!'));
